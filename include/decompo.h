aecf67b1fe920de92cbe8fa80932956be7018ede
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-05-06 09:19:02 +0200

    en-têtes

--- include/decompo.h
+++ include/decompo.h
@@ -1,3 +1,8 @@
+/* This program is free software. It comes WITHOUT ANY WARRANTY, to
+* the extent permitted by applicable law. You can redistribute it
+* and/or modify it under the terms of the Do What The Fuck You Want
+* To Public License, Version 2, as published by Sam Hocevar. See
+* http://wtfpl.net for more details. */
 /**
  * \file lu.h
  * \brief Décomposition en LU (header)
@@ -8,6 +13,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "base.h"
 #include "matrix.h"
 #include "operations.h"
--- src/operations.c
+++ src/operations.c
@@ -1,3 +1,8 @@
+/* This program is free software. It comes WITHOUT ANY WARRANTY, to
+* the extent permitted by applicable law. You can redistribute it
+* and/or modify it under the terms of the Do What The Fuck You Want
+* To Public License, Version 2, as published by Sam Hocevar. See
+* http://wtfpl.net for more details. */
 /**
  * \file operations.c
  * \brief Opérations sur les matrices (code)
